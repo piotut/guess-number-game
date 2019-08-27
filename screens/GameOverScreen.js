a4ef6c7322f716aff1768726cd63c5b753b0cfb1
Piotr Tutak <piotut@gmail.com>
2019-08-27 14:59:50 +0200

    Game over screen

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    Button,
+} from 'react-native';
+
+const GameOverScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text>The Game is Over!</Text>
+            <Text>Number of rounds: {props.roundsNumber}</Text>
+            <Text>Number was: {props.userNumber}</Text>
+            <Button title="New Game" onPress={props.onRestart}/>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export default GameOverScreen;
